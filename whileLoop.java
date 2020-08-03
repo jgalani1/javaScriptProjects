dabc255ce7f5d29f6c3c68bbce82bf1214f2cafa
GZVJP3@ALLYIPOC.COM <GZVJP3@ALLYIPOC.COM>
2020-08-03 10:23:38 -0400

    while

---
+++ whileLoop.java
@@ -0,0 +1,16 @@
+public class whileLoop {
+
+    public static void main(String[] args) {
+        // write your code here
+        int sum = 0;
+        int j = 3;
+        while (j <= 79){
+            sum = sum + j;
+            j++;
+            System.out.println(sum);
+        }
+        System.out.println("The last value of j is: " + j);
+        // System.out.println(sum);
+    }
+
+}
