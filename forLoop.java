60cb0fafec55288d322866b01019849c8b892633
GZVJP3@ALLYIPOC.COM <GZVJP3@ALLYIPOC.COM>
2020-08-03 09:56:16 -0400

    forLoop

---
+++ forLoop.java
@@ -0,0 +1,17 @@
+public class forLoop {
+
+    public static void main(String[] args) {
+        // write your code here
+        int sum = 0;
+        int j = 0;
+        for(j =3; j<= 79; j++)
+        {
+            double d = 37.93;
+            sum = sum + j;
+            // System.out.println(sum);
+        }
+        System.out.println(j);
+        System.out.println(sum);
+    }
+
+}
