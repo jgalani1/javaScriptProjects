3445287bd5122e9c1af30b1dd9dfebf577df9a05
GZVJP3@ALLYIPOC.COM <GZVJP3@ALLYIPOC.COM>
2020-08-03 10:22:08 -0400

    substring

---
+++ substrings.java
@@ -0,0 +1,9 @@
+public class substrings {
+    public static void main(String args []){
+
+        //This prints: ky the d
+        String myPet = "Sparky the dog";
+        String substring = myPet.substring(4,12);
+        System.out.println(substring);
+    }
+}
