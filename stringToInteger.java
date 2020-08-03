07712cc1ccfa14e06ddaf15fa3eb1218ff7ec364
GZVJP3@ALLYIPOC.COM <GZVJP3@ALLYIPOC.COM>
2020-08-03 10:27:45 -0400

    integer

---
+++ stringToInteger.java
@@ -0,0 +1,15 @@
+public class stringToInteger {
+    public static void main (String args []) {
+
+        //Converting a string to integer
+        String s = "169";
+        int num = Integer.parseInt(s);
+        System.out.println(num);
+
+        //Converting a string to a double
+        String x = "197.54";
+        double dec = Double.parseDouble(x);
+        System.out.println(dec);
+
+    }
+}
